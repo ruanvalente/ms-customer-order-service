5d7f2c74c0fb3f699fd5ade8b9e4fe2d9b6a5939
ruanvalente <ruanopensuselinux@gmail.com>
2025-04-10 21:48:16 -0300

    feat: update DatabaseModule and Customer entity to include Orders relationship

--- src/modules/customer/entities/customer.entity.ts
+++ src/modules/customer/entities/customer.entity.ts
@@ -1,16 +1,20 @@
-import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+import { Orders } from 'src/modules/orders/entities/orders.entity';
+import { Column, Entity, OneToMany, PrimaryGeneratedColumn } from 'typeorm';
 
 @Entity('tb_customer')
 export class Customer {
-  @PrimaryGeneratedColumn()
-  id: number;
+	@PrimaryGeneratedColumn()
+	id: number;
 
-  @Column({ nullable: false })
-  name: string;
+	@Column({ nullable: false })
+	name: string;
 
-  @Column({ unique: true, nullable: false })
-  email: string;
+	@Column({ unique: true, nullable: false })
+	email: string;
 
-  @Column({ nullable: true })
-  phoneNumber: string;
+	@Column({ nullable: true })
+	phoneNumber: string;
+
+	@OneToMany(() => Orders, (order) => order.customer)
+	orders: Orders[];
 }

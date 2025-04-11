847f860bf67537391f5973b77c170facc86c5fcb
ruanvalente <ruanopensuselinux@gmail.com>
2025-04-11 11:26:13 -0300

    feat: add OrderItem entity and update Orders entity to include items relationship

--- src/modules/orders/entities/orders.entity.ts
+++ src/modules/orders/entities/orders.entity.ts
@@ -1,27 +1,50 @@
+import { ApiProperty } from '@nestjs/swagger';
+import { IsDate, IsInt, IsOptional, IsPositive } from 'class-validator';
 import { Customer } from 'src/modules/customer/entities/customer.entity';
 import {
 	Column,
 	Entity,
 	JoinColumn,
 	ManyToOne,
+	OneToMany,
 	PrimaryGeneratedColumn,
 } from 'typeorm';
 import { OrderStatus } from './enum/order.status';
+import { OrderItem } from './order-item.entity';
 
 @Entity('tb_orders')
 export class Orders {
+	@ApiProperty({ example: 1, description: 'Unique identifier of the order' })
 	@PrimaryGeneratedColumn()
 	id: number;
 
+	@ApiProperty({
+		example: 123,
+		description: 'Customer ID associated with the order',
+	})
+	@IsInt()
+	@IsPositive()
 	@Column()
 	clientId: number;
 
+	@ApiProperty({
+		example: '2025-04-11T12:00:00Z',
+		description: 'Date and time when the order was created',
+		required: false,
+	})
+	@IsOptional()
+	@IsDate()
 	@Column({
 		type: 'timestamp with time zone',
 		nullable: true,
 	})
 	createdAt: Date;
 
+	@ApiProperty({
+		enum: OrderStatus,
+		example: OrderStatus.PENDING,
+		description: 'Current status of the order',
+	})
 	@Column({
 		type: 'enum',
 		enum: OrderStatus,
@@ -29,9 +52,22 @@ export class Orders {
 	})
 	status: OrderStatus;
 
+	@ApiProperty({
+		type: () => Customer,
+		description: 'Customer entity related to the order',
+	})
 	@ManyToOne(() => Customer, (customer) => customer.orders, {
 		onDelete: 'CASCADE',
 	})
 	@JoinColumn({ name: 'clientId' })
 	customer: Customer;
+
+	@ApiProperty({
+		type: () => [OrderItem],
+		description: 'List of items included in the order',
+	})
+	@OneToMany(() => OrderItem, (item) => item.order, {
+		cascade: true,
+	})
+	items: OrderItem[];
 }
